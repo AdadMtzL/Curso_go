ad38beb5e3f629ac8bf8ea6944f97127cbf9be35
Adad Marcel <adadmarcelmartinez@gmail.com>
2023-06-18 11:49:26 -0600

    Comentación del codigo inicial

--- holamundo/main.go
+++ holamundo/main.go
@@ -7,12 +7,10 @@ package main
 //que pedo, es con git ahuevo esto
 import (
 	"Users/19286463/Desktop/Cursos/Desarrollodesde0/seccion8_Go/Curso_go/holamundo/Ejercicios"
-	"Users/19286463/Desktop/Cursos/Desarrollodesde0/seccion8_Go/Curso_go/holamundo/Variables"
 	"fmt"
-
 	//strconv es un paquete para conversion a string
 	//poner el path completo del proyecto
-	"runtime" //paqueet para detectar cosas del sistema
+	//paqueet para detectar cosas del sistema
 )
 
 // Las funciones que se pueden acceder desde eafurea son Nombre y las internas son nombre
@@ -22,14 +20,15 @@ func main() {
 	var str string
 	var no int
 	fmt.Println("Primer mensaje")
-	Variables.Numenteros()
-	Variables.Resta()
-	fmt.Println("Resta con parametros: ", Variables.Restaparametros(55, 12))
-	if os := runtime.GOOS; os == "Linux." { //obtencion del sistema operativo y su comparacion eficiente
-		fmt.Println("Sisema Linux o ios, no se la verdad")
-	} else {
-		fmt.Println("Sisema windows")
-	}
+
+	//	Variables.Numenteros()
+	//	Variables.Resta()
+	//	fmt.Println("Resta con parametros: ", Variables.Restaparametros(55, 12))
+	//	if os := runtime.GOOS; os == "Linux." { //obtencion del sistema operativo y su comparacion eficiente
+	//		fmt.Println("Sisema Linux o ios, no se la verdad")
+	//	} else {
+	//		fmt.Println("Sisema windows")
+	//	}
 	//para ponder anidas poner el if despues del else
 
 	/**
