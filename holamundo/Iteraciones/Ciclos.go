6bc9fcd11be67e1fc0e1d10c777b5986d75dbacd
Adad Marcel <adadmarcelmartinez@gmail.com>
2023-06-30 10:50:49 -0600

    Adición de comentarios a las iteraciones en go

--- holamundo/Iteraciones/Ciclos.go
+++ holamundo/Iteraciones/Ciclos.go
@@ -14,7 +14,10 @@ func CicloFor() {
 	}
 	**/
 	i := 0
-	for i < 10 {
+	for i < 10 { //i:=0;i<10;i++ es lo mismo i+=n incrementa en n
+		//Se puede combinar con multiples estructuras como las
+		//Condicionales asignación de variables y cosa así
+		//hay instrucciones como break, continue, entre otras
 		fmt.Println(i)
 		i++
 	}
