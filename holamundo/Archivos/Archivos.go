b97108bcb46c153e508f6c3d00a8b17e40141164
Adad Marcel <adadmarcelmartinez@gmail.com>
2023-07-02 13:28:13 -0600

    Correcion de entrada de datos vacía del archivo

--- holamundo/Archivos/Archivos.go
+++ holamundo/Archivos/Archivos.go
@@ -7,19 +7,22 @@ import (
 	//"ioutil"
 )
 
-var texto string
-var err error
-
-func Arch() {
-	fmt.Println("Manejo de archivos")
-
+func datos() string {
+	var texto string
 	for i := 1; i < 11; i++ {
 		texto += fmt.Sprintf("%d * 4 = %d", i, i*4)
 	}
+	return texto
+}
+
+func Arch() {
+	fmt.Println("Manejo de archivos")
+	var textoF string
+	textoF = datos()
 	archivo, err := os.Create("./Archivos/tabla.txt")
 	if err != nil {
 		panic("Error al crear el archivo")
 	}
-	fmt.Println(archivo, texto)
+	fmt.Println(archivo, textoF)
 	archivo.Close()
 }
