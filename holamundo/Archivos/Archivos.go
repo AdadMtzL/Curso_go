ab4202522b6edb0e90a944866e00c121e7e7b246
Adad Marcel <adadmarcelmartinez@gmail.com>
2023-07-02 13:19:16 -0600

    creación de un txt

--- holamundo/Archivos/Archivos.go
+++ holamundo/Archivos/Archivos.go
@@ -2,15 +2,24 @@ package archivos
 
 import (
 	"fmt"
+	"os"
+	//"bufio"
+	//"ioutil"
 )
 
 var texto string
+var err error
 
-func Arch() string {
+func Arch() {
 	fmt.Println("Manejo de archivos")
 
 	for i := 1; i < 11; i++ {
 		texto += fmt.Sprintf("%d * 4 = %d", i, i*4)
 	}
-	return texto
+	archivo, err := os.Create("./Archivos/tabla.txt")
+	if err != nil {
+		panic("Error al crear el archivo")
+	}
+	fmt.Println(archivo, texto)
+	archivo.Close()
 }
