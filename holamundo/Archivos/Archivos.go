7d132f3502adf116ec837b8ffc9b38685eacda65
Adad Marcel <adadmarcelmartinez@gmail.com>
2023-07-03 12:40:04 -0600

    màs notas

--- holamundo/Archivos/Archivos.go
+++ holamundo/Archivos/Archivos.go
@@ -28,4 +28,7 @@ func Arch() {
 	//Para abrir un archivo esarch,err := os.OpenFile(FileName,Os.O_WRONLY | os.O_APPEND,permisos tipolinux) Esto es para cuando un archivo pueda seguir recibiendo texto
 	//Escritura de texto tambien se puede  con arch.WriteString(texto)
 	//Siempre cerrar el archivo
+	//para leer archivos es os.Open(filename)
+	// scanner := bufio.newscanner(archivo), luego imprimir mediante for y cerrar el archivo
+	//for scanner.scan { letras := scanner.text print letras}
 }
