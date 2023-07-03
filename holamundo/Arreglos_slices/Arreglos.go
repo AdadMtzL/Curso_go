eb845b6df46796a8b5e82de96751e527d26e237f
Adad Marcel <adadmarcelmartinez@gmail.com>
2023-07-03 13:48:08 -0600

    arreglos y slices

--- holamundo/Arreglos_slices/Arreglos.go
+++ holamundo/Arreglos_slices/Arreglos.go
@@ -17,8 +17,16 @@ func MostrarArreglo() {
 /**
 *Slices
 **/
+var arreglo [10]int = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
 var tb []int = []int{1, 3, 4, 2, 54, 2} //No se define el tamaño del arreglo
 func Slcs() {
 	fmt.Println(tb)
 	//El slice ajusta las dimensiones del arreglos de manera automatica
+	//Conversion de un arreglo a slice
+	porcion := arreglo[3:]   //3 en adelante
+	porcion2 := arreglo[:5]  //0 a 5
+	porcion3 := arreglo[2:5] //2 a 5
+	fmt.Println(porcion)
+	fmt.Println(porcion2)
+	fmt.Println(porcion3)
 }
